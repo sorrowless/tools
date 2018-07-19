46e4eccff1338d443ac5d58c04e78d85fe78cd13
Stanislaw Bogatkin <sbog@sbog.ru>
2018-07-19 15:38:35 +0300

    Add sshhost tool
    Create tool to fast search in SSH config.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tools
 Small tools I use in everyday work
+
+`sshhost.py` - tool to fast search in ssh config. There are moments when someone just tell me host IP and I don't remember its config name, so each time I need to grep it from config. This tool allow to do so by just giving to it a part of anything return whole information about needed host if such exists in config.
---
+++ sshhost.py
@@ -0,0 +1,43 @@
+import os
+import pprint
+import re
+import sys
+
+
+args = sys.argv[1:]
+if len(args) != 1:
+    print('Usage: sshhost <part of hostname or IP address>')
+    sys.exit(1)
+search_string = args[0]
+re_string = re.compile(r'(.*)' + search_string + r'(.*)')
+
+data = ''
+with open(os.path.expanduser('~/.ssh/config'), 'rt') as f:
+    data = f.readlines()
+
+hosts = list()
+host = dict()
+for row in data:
+    if row.isspace():
+        continue
+    key, value = row.strip().split(' ')
+    if key.lower() == 'host':
+        hosts.append(host)
+        host = dict()
+    host[key.lower()] = value.lower()
+hosts.append(host)
+
+found = list()
+for host in hosts:
+    for item in host.values():
+        if re_string.match(item):
+            found.append(host)
+
+if found:
+    for host in found:
+        print()
+        pprint.pprint(host)
+    sys.exit(0)
+
+print('Nothing found')
+sys.exit(1)
