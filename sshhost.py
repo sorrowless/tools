35dca575d1324482081bd1cd2dd2fca0e1f162eb
sorrowless <sbog@sbog.ru>
2019-06-02 12:21:42 +0300

    Add JSON output opportunity
    Also move to argparse module to parse arguments as there is more than
    one now.

--- sshhost.py
+++ sshhost.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
+
+import argparse
+import json
 import os
 import pprint
 import re
 import sys
 
 
-args = sys.argv[1:]
-if len(args) != 1:
-    print('Usage: sshhost <part of hostname or IP address>')
-    sys.exit(1)
-search_string = args[0]
-re_string = re.compile(r'(.*)' + search_string + r'(.*)')
+parser = argparse.ArgumentParser(
+    description='Process SSH config file and search for data in it.')
+parser.add_argument('searchstring', type=str, default='.*',
+                    help='A string for search')
+parser.add_argument('-j', '--json', action='store_true',
+                    help='Print an output in valid JSON format')
+
+args = parser.parse_args()
+re_string = re.compile(r'(.*)' + args.searchstring + r'(.*)')
 
 data = ''
 with open(os.path.expanduser('~/.ssh/config'), 'rt') as f:
@@ -34,12 +40,14 @@ for host in hosts:
         if re_string.match(item):
             found.append(host)
 
-if found:
-    uniq = list({v['host']:v for v in found}.values())
+uniq = list({v['host']: v for v in found}.values())
+if uniq and not args.json:
     for host in uniq:
         print()
         pprint.pprint(host)
-    sys.exit(0)
-
-print('Nothing found')
-sys.exit(1)
+elif uniq:
+    print(json.dumps(uniq, indent=2))
+else:
+    print('Nothing found')
+    sys.exit(1)
+sys.exit(0)
